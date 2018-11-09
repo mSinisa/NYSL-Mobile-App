d6b376f7c2d6bfcda97f572ac180709c2d45d41a
mSinisa <msinisa602@yahoo.com>
2018-11-09 18:04:46 +0100

    Second repository

--- JS/info.js
+++ JS/info.js
@@ -169,7 +169,10 @@ var info = {
         gp: 6,
         won: 4,
         draw: 1,
-        lost: 1
+        lost: 1,
+        address: "24 W. Walton St., Chicago, IL 60610",
+        stadium_name:"La Bombonera",
+        mapsrc:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.6542135574196!2d-87.6312383852653!3d41.90029307922041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd34e07f6bac3%3A0x68a82e5d59952c86!2s24+W+Walton+St%2C+Chicago%2C+IL+60610%2C+USA!5e0!3m2!1sen!2ses!4v1541776203124"
 
     }, {
         Name: "Green Snakes",
@@ -178,7 +181,10 @@ var info = {
         gp: 6,
         won: 0,
         draw: 0,
-        lost: 6
+        lost: 6,
+        address: "1734 N. Orleans St., Chicago, IL 60614",
+        stadium_name: "Celtic Park",
+        mapsrc:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.0258285480745!2d-87.64002868526472!3d41.91380347921943!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd34073f306a3%3A0x9e1726bbf8f23f0e!2s1734+N+Orleans+St%2C+Chicago%2C+IL+60614%2C+USA!5e0!3m2!1sen!2ses!4v1541776524378"
         
     }, {
         Name: "Horned Frogs",
@@ -187,7 +193,11 @@ var info = {
         gp: 6,
         won: 2,
         draw: 3,
-        lost: 1
+        lost: 1,
+        address: "2245 N. Southport Ave., Chicago, IL 60614",
+        stadium_name: "Stadio Artemio Franchi",
+        mapsrc:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.585683085622!2d-87.66511458526429!3d41.92326457921858!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f9b8d2d%3A0x62ad8b907dd755d6!2s2245+N+Southport+Ave%2C+Chicago%2C+IL+60614%2C+USA!5e0!3m2!1sen!2ses!4v1541776607845"
+        
     }, {
         Name: "Jaguars",
         logo: "northside_jaguars_logo",
@@ -195,7 +205,10 @@ var info = {
         gp: 6,
         won: 1,
         draw: 2,
-        lost: 3
+        lost: 3,
+        address: "2625 N. Orchard St., Chicago, IL 6061",
+        stadium_name: "Estádio José Alvalade",
+        mapsrc:"https://www.google.com/maps/embed?pb="
     }, {
         Name: "Red Raiders",
         logo: "red_raiders_logo",
@@ -203,7 +216,11 @@ var info = {
         gp: 6,
         won: 5,
         draw: 1,
-        lost: 0
+        lost: 0,
+        address: "1409 N. Ogden Ave., Chicago, IL 60610",
+        stadium_name:"Anfield",
+        mapsrc:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.336488231587!2d-87.64835588526502!3d41.90712467921986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd33af13a8945%3A0xb6ad1ec2b6f379ba!2s1409+N+Ogden+Ave%2C+Chicago%2C+IL+60610%2C+USA!5e0!3m2!1sen!2ses!4v1541776731509" 
+        
     }, {
         Name: "Golden Eagles",
         logo: "golden_eagles_logo",
@@ -211,7 +228,10 @@ var info = {
         gp: 6,
         won: 1,
         draw: 3,
-        lost: 2
+        lost: 2,
+        address: "2101 N. Fremont St., Chicago, IL 60614",
+        stadium_name:"Mestalla Stadium",
+        mapsrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.7480203091445!2d-87.65355788526448!3d41.919775279218904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd3196fb41dc7%3A0x970be7f7d6336df5!2s2101+N+Fremont+St%2C+Chicago%2C+IL+60614%2C+USA!5e0!3m2!1sen!2ses!4v1541776777327"
     }]
 
 
--- JS/standings.js
+++ JS/standings.js
@@ -4,19 +4,41 @@ var app = new Vue({
         week: "",
         info: info.league_schedule,
         teams: info.teams,
+
+    },
+
+    methods: {
         getTeamRanking: function () {
             var sortedTeams = Array.from(this.teams);
             sortedTeams.sort(function (a, b) {
                 return (a["pts"] < b["pts"]) ? 1 : ((b["pts"] < a["pts"]) ? -1 : 0);
             });
             return sortedTeams;
+        },
+
+        getLogo: function (team) {
+            if (team.logo.match(/blue_devils_logo/g)) {
+                return "/styles/images/blue_devils_logo.png";
+            } else if (team.logo.match(/golden_eagles_logo/g)) {
+                return "styles/images/golden_eagles_logo.png";
+            } else if (team.logo.match(/green_snakes_logo/g)) {
+                return "styles/images/green_snakes_logo.png";
+            } else if (team.logo.match(/horned_frogs_logo/g)) {
+                return "styles/images/horned_frogs_logo.png";
+            } else if (team.logo.match(/northside_jaguars_logo/g)) {
+                return "styles/images/northside_jaguars_logo.png";
+            } else {
+                return "/styles/images/red_raiders_logo.png";
+            }
         }
     },
+
     computed: {
 
     }
 
 });
+
 $('.slidingMenu').slick({
     centerMode: true,
     centerPadding: '60px',
